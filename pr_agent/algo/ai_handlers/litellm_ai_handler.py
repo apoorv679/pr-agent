41058a467c4f6ba529ab986f168db16985d129c5
apoorv-clear <apoorv.agrawal@clear.in>
2025-01-15 12:49:25 +0530

    testing

--- pr_agent/algo/ai_handlers/litellm_ai_handler.py
+++ pr_agent/algo/ai_handlers/litellm_ai_handler.py
@@ -238,7 +238,7 @@ class LiteLLMAIHandler(BaseAiHandler):
                 get_logger().info(f"\nUser prompt:\n{user}")
 
             litellm.api_base = "https://llm-proxy.internal.cleartax.co/openai/v1"
-            kwargs.api_base = "https://llm-proxy.internal.cleartax.co/openai/v1"
+            kwargs["api_base"] = "https://llm-proxy.internal.cleartax.co/openai/v1"
 
             print("KWARGS", kwargs)
             print("OPEN_AI_BASE_URL", litellm.api_base)
